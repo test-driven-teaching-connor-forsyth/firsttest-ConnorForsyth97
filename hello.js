582ff9a60bbb08ba43fccba63dd5689ee29856a6
ConnorForsyth <connorforsyth34@gmail.com>
2020-04-24 15:04:04 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -7,4 +7,5 @@ http.createServer(function (req, res) {
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
-var stopError = "";
+var example = "A string in javascript";
+console.log(example);
